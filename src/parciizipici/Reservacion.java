c391ad5530af99cdf9aa2f6e3e091ba9a5f9cf54
Cristian Mundo <00086917@uca.edu.sv>
2018-05-02 14:51:28 -0600

    Creacion de la clase Reservacion

---
+++ src/parciizipici/Reservacion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parciizipici;
+
+/**
+ *
+ * @author cmundo
+ */
+public class Reservacion {
+    
+    int idHuesped;
+    String idPaquete;
+    int Fecha_ingreso;
+    int Fecha_salida;
+    int numeroTarjetaCredito;
+    float monto;
+    
+    
+    
+    
+    
+    
+}
