57429553d3006970a4974a2becac80337e8776e0
Rodolfo Salgado <37157142+AlexanderSalgado@users.noreply.github.com>
2018-05-02 15:07:15 -0600

    Creacion de clase temporada.

--- src/parciizipici/Temporada.java
+++ src/parciizipici/Temporada.java
@@ -1,21 +1,14 @@
-package parciizipici;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package parciizipici;
 
 /**
  *
  * @author rodol
  */
 public class Temporada {
-    
-    String id, fechaInicio, FechaFin;
-    
-    
-    
-    
     
 }
