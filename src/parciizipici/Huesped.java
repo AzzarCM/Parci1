32afe91674eec262247ca634ec5a5834a6642e51
Margarita <00225117@uca.edu.sv>
2018-05-02 15:22:53 -0600

    correccion clases

---
+++ src/parciizipici/Huesped.java
@@ -0,0 +1,36 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Margarita
+ */
+public class Huesped {
+    
+    String nombre, apellido;
+    int DUI;
+    
+    public void agregarHuesped(){
+        
+    }
+    
+    public void modificarHuesped(){
+        
+    }
+    
+    public void eliminarHuesped(){
+        
+    }
+    
+    public void consultarHuesped(){
+        
+    }
+    
+    public void consultarReserva(){
+        
+    }
+}
---
+++ src/parciizipici/Servicios.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Margarita
+ */
+public class Servicios {
+        
+int id;
+String descripcion;
+String fecha_Alta;
+String fecha_Modificación;
+
+public void agregarServicio(){
+    
+}
+
+public void consultarServicio(){
+    
+}
+
+public void modificarServicio(){
+    
+}
+
+public void eliminarServicio(){
+    
+}
+
+
+
+    
+}
