4345b3870b2278623ab21307a6f44fe6fa9fe93d
Rodolfo Salgado <00191617@uca.edu.sv>
2018-05-02 15:02:44 -0600

    Creacion de clase temporada.

---
+++ src/parciizipici/Temporada.java
@@ -0,0 +1,21 @@
+package parciizipici;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rodol
+ */
+public class Temporada {
+    
+    String id, fechaInicio, FechaFin;
+    
+    
+    
+    
+    
+}
